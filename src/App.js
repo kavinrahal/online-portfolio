8153e580a26dd2766223f50ba6ba5a4e6b2d9174
Kavin Abeysinghe <s3756717@student.rmit.edu.au>
2022-03-28 03:09:34 +1100

    Contact section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './Components/NavBar';
 import Projects from './Components/Projects';
 import Skills2 from './Components/Skills2';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import More from './Components/More';
 
 function App() {
   return (
@@ -16,13 +17,15 @@ function App() {
       <Skills2/>
       <Projects />
       <Experience />
+      <More />
       <Router>
         <Routes>
           <Route path='/landing' component={<Landing/>}/>     
           <Route path='/aboutMe' component={<AboutMe/>}/>     
           <Route path='/skills' component={<Skills2/>}/>
           <Route path='/projects' component={<Projects/>}/>
-          <Route path='/experience' component={<Experience/>}/>     
+          <Route path='/experience' component={<Experience/>}/>   
+          <Route path='more' component={<More/>} />
         </Routes>
       </Router>
     </div>
--- src/Components/More.js
+++ src/Components/More.js
@@ -1,4 +1,35 @@
 import './AboutMe.css';
 import './Projects.css'
+import './More.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import linkedIn from '../linkedin.png';
+import gitHub from '../github.png';
+import facebook from '../facebook.png';
+import insta from '../insta.png';
+
+function More(){
+    AOS.init();
+    return(
+        <div className='more'>
+            <head>
+                <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
+            </head>
+            <script src="bower_components/aos/dist/aos.js"></script> 
+            <div className='moreData' data-aos="fade-up" data-aos-duration="1000">
+                <h1 className = "aboutMeText">Want to know more?</h1>
+                <br></br>
+                <br></br>
+                <div className = "linksRow">
+                    <a href = "https://www.linkedin.com/in/kavinrahal/" target="_blank" rel="noopener noreferrer"><img className='logoImg' src = {linkedIn}/></a>
+                    <a href = "https://github.com/kavinrahal" target="_blank" rel="noopener noreferrer"><img className='logoImg' src = {gitHub}/></a>
+                    <a href = "https://www.facebook.com/kavindya.abeysinghe.1/" target="_blank" rel="noopener noreferrer"><img className='logoImg' src = {facebook}/></a>
+                    <a href = "https://www.instagram.com/freeekonaleash/" target="_blank" rel="noopener noreferrer"><img className='logoImg' src = {insta}/></a>
+                </div>
+                <p className='rights'>© Kavindya Abeysinghe 2022. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
+
+export default More;
