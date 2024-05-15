c536e94b1fe2789ed0a37bcdcbec51cd9cfc09e2
Kavin Abeysinghe <s3756717@student.rmit.edu.au>
2024-05-15 15:51:58 +1000

    Commit to trigger deployment

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -23,7 +23,7 @@ function AboutMe(){
             <div className = "column" id = "rightSide">
                 <div data-aos="fade-up" data-aos-duration="1000" className = "aboutText">
                     <p>Hey there! I’m Kavindya, an IT graduate from the Royal Melbourne Institute of Technology, currently with almost 2 years of experience. As my time as a full stack developer, I mainly worked 
-                    on the payments team and focused on seamlessly connecting robust GraphQL and ASP.NetCore back-end infrastructure with React front-end.</p>
+                    on the payments team, and while my primary interest is front-end development I focused on seamlessly connecting robust GraphQL and ASP.NetCore back-end infrastructure with React front-end.</p>
                     <p>A bit about myself, I’m very much a team player and love getting to know new people and share my experiences with them and hearing their experiences and points of views as well. 
                     New perspectives fascinate me, and I love looking at things through these different perspectives, which helps me with the designing process that is very important for UI design.</p>
                     <p>My work ethic is simple. I simply will not rest, till my responsibilities have been fulfilled, regardless of how daunting the task may be. Willing to take up all types of challenges, 
